355d3ddaa8cb94338525f76a770f08608c6f96a9
Lucas Lemos <llcf@ic.ufal.br>
2022-09-15 12:03:43 -0300

    bringup digital input

--- bringup/bringup.ino
+++ bringup/bringup.ino
@@ -14,7 +14,7 @@
  * digital.
  *
  */
-#define BRINGUP_DIGITAL_INPUT 0
+#define BRINGUP_DIGITAL_INPUT 1
 /**
  * @brief Se estiver setado para 1, o teste de bancada a se fazer é o de saída
  * digital.
@@ -26,7 +26,7 @@
  * analógica.
  *
  */
-#define BRINGUP_ANALOG_INPUT 1
+#define BRINGUP_ANALOG_INPUT 0
 /**
  * @brief Se estiver setado para 1, o teste de bancada a se fazer é o de saída
  * analógica.
@@ -55,11 +55,87 @@
 #if ARDUINO_MEGA == 1
 #define FIRST_ADC_PIN 54
 #define ADC_AMOUNT 16
+#define DIGITAL_AMOUNT  54
+typedef enum {
+  D_PIN0 = 2,
+  D_PIN1 = 3,
+  D_PIN2 = 6,
+  D_PIN3 = 7,
+  D_PIN4 = 1,
+  D_PIN5 = 5,
+  D_PIN6 = 15,
+  D_PIN7 = 16,
+  D_PIN8 = 17,
+  D_PIN9 = 18,
+  D_PIN10 = 23,
+  D_PIN11 = 24,
+  D_PIN12 = 25,
+  D_PIN13 = 26,
+  D_PIN14 = 64,
+  D_PIN15 = 63,
+  D_PIN16 = 13,
+  D_PIN17 = 12,
+  D_PIN18 = 46,
+  D_PIN19 = 45,
+  D_PIN20 = 40,
+  D_PIN21 = 43,
+  D_PIN22 = 78,
+  D_PIN23 = 77,
+  D_PIN24 = 76,
+  D_PIN25 = 75,
+  D_PIN26 = 74,
+  D_PIN27 = 73,
+  D_PIN28 = 72,
+  D_PIN29 = 71,
+  D_PIN30 = 60,
+  D_PIN31 = 59,
+  D_PIN32 = 58,
+  D_PIN33 = 57,
+  D_PIN34 = 56,
+  D_PIN35 = 55,
+  D_PIN36 = 54,
+  D_PIN37 = 53,
+  D_PIN38 = 50,
+  D_PIN39 = 70,
+  D_PIN40 = 52,
+  D_PIN41 = 51,
+  D_PIN42 = 42,
+  D_PIN43 = 41,
+  D_PIN44 = 40,
+  D_PIN45 = 39,
+  D_PIN46 = 38,
+  D_PIN47 = 37,
+  D_PIN48 = 36,
+  D_PIN49 = 35,
+  D_PIN50 = 22,
+  D_PIN51 = 21,
+  D_PIN52 = 20,
+  D_PIN53 = 19
+} digital_pin;
 
 #elif ARDUINO_MEGA == 0
 #define FIRST_ADC_PIN 14
 #define ADC_AMOUNT 6
+#define DIGITAL_AMOUNT  14
+typedef enum{
+  D_PIN0 = 0,
+  D_PIN1 = 1,
+  D_PIN2 = 2,
+  D_PIN3 = 3,
+  D_PIN4 = 4,
+  D_PIN5 = 5,
+  D_PIN6 = 6,
+  D_PIN7 = 7,
+  D_PIN8 = 8,
+  D_PIN9 = 9,
+  D_PIN10 = 10,
+  D_PIN11 = 11,
+  D_PIN12 = 12,
+  D_PIN13 = 13
+}digital_pin;
 #endif
+
+
 /**
  * @brief Teste de bancada de entrada digital
  *
@@ -190,7 +266,29 @@ void bringup_analog_input() {
   for (int i = 0; i < ADC_AMOUNT; i++)
   {
     Serial.print("ADC "+String(i)+": ");
-    Serial.print(String(adc[i])+"\n");
+    Serial.println(String(adc[i]));
+  }
+  Serial.println("********************");
+  delay(1000);
+}
+
+void setup_digital_input(){
+  Serial.begin(9600);
+  for (uint8_t i = 0; i < DIGITAL_AMOUNT; i++) {
+    pinMode(i, INPUT);
+  }
+}
+
+void bringup_digital_input(){
+  uint8_t digital[DIGITAL_AMOUNT];
+
+  for (uint8_t i = 0; i < DIGITAL_AMOUNT; i++) {
+    digital[i] = digitalRead(i);
+  }
+  for (int i = 0; i < DIGITAL_AMOUNT; i++)
+  {
+    Serial.print("D_PIN "+String(i)+": ");
+    Serial.println(String(digital[i]));
   }
   Serial.println("********************");
   delay(1000);
