a07c4684156aff3461bb986e148d09f7c0cad0a4
Lucas Lemos <llcf@ic.ufal.br>
2022-12-13 19:35:36 -0300

    state machine

--- source/estacionamento/estacione/estacione.ino
+++ source/estacionamento/estacione/estacione.ino
@@ -1,32 +1,45 @@
 #include <HCSR04.h>
-
-// C++ code
 #include <Servo.h>
 #include <Ultrasonic.h>
 #include <LiquidCrystal_I2C.h>
 
+/* Definições Úteis */
 #define sensor1TriggerPin 2
 #define sensor1EchoPin 3
-
 #define sensor2TriggerPin 4
 #define sensor2EchoPin 5
-
 #define sensor3TriggerPin 6
 #define sensor3EchoPin 7
-
 #define sensor4TriggerPin 8
 #define sensor4EchoPin 9
-
 #define insideButtonPin 13
 #define outsideButtonPin 12
-
 #define angClose 90
 #define angOpen 20
 #define servoPin 11
 
+typedef enum {
+  WAITING_ACTION,
+  BUTTON_PRESSED,
+  RUNNING,
+  DONE,
+} PARKING_STATE;
+
 Servo servo;
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+UltraSonicDistanceSensor ultrasonic1(sensor1TriggerPin, sensor1EchoPin);
+UltraSonicDistanceSensor ultrasonic2(sensor2TriggerPin, sensor2EchoPin);
+UltraSonicDistanceSensor ultrasonic3(sensor3TriggerPin, sensor3EchoPin);
+UltraSonicDistanceSensor ultrasonic4(sensor4TriggerPin, sensor4EchoPin);
+
+UltraSonicDistanceSensor sensors[4] = {ultrasonic1, ultrasonic2, ultrasonic3, ultrasonic4};
+
+PARKING_STATE state = WAITING_ACTION;
+
+String buttonPressed = "inside";
 int currentAngleServo = angClose;
-int sensorDebug = 1;
 int parkingSpacesCount = -1;
 
 class ParkingSpace {
@@ -47,22 +60,119 @@ class ParkingSpace {
     };
 };
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-UltraSonicDistanceSensor ultrasonic1(sensor1TriggerPin, sensor1EchoPin);
-UltraSonicDistanceSensor ultrasonic2(sensor2TriggerPin, sensor2EchoPin);
-UltraSonicDistanceSensor ultrasonic3(sensor3TriggerPin, sensor3EchoPin);
-UltraSonicDistanceSensor ultrasonic4(sensor4TriggerPin, sensor4EchoPin);
-
-
-
-UltraSonicDistanceSensor sensors[4] = {ultrasonic1, ultrasonic2, ultrasonic3, ultrasonic4};
-
+ParkingSpace bestSpot = ParkingSpace(-1, -1, "Nenhuma vaga", "Nenhuma vaga");
 ParkingSpace parkingSpaces[4] = { 
  ParkingSpace(0, 3, "B4", "S7RGRBRBGE"),
  ParkingSpace(1, 4, "B1", "S4RGRGE"), 
  ParkingSpace(2, 2, "A1", "S3RBGE"), 
  ParkingSpace(3, 1, "A2", "S4RBRGE")  
 };
+
+void waiting_action_exec();
+void button_pressed_exec();
+void running_exec();
+void done_exec();
+void clearRowLcd(int row);
+void moveGate(int moviment);
+float getMean(float array[], int size);
+float getSensorDistance(UltraSonicDistanceSensor sensor);
+int freeParkingSpace(UltraSonicDistanceSensor ultrasonic);
+ParkingSpace getBestParkingSpace();
+void openGate(int timeToCLose);
+void openGateButtonPressed(String button);
+void updateParkingSpacesNumber();
+void countParkingSpaces();
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(insideButtonPin, INPUT);
+  pinMode(outsideButtonPin, INPUT);
+  servo.attach(servoPin);
+  servo.write(90);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("EstaciOne");
+}
+
+void loop()
+{     
+    bestSpot = getBestParkingSpace();
+    if(bestSpot.sensorId != -1){
+      Serial.println(bestSpot.path);
+    }
+    countParkingSpaces();
+    
+    switch(state){
+      case WAITING_ACTION:
+        waiting_action_exec();
+        break;
+      case BUTTON_PRESSED:
+        button_pressed_exec();
+        break;
+      case RUNNING:
+        running_exec();
+        break;
+      case DONE:
+        done_exec();
+        break;
+    }
+    
+    
+
+    // float distance1 = getSensorDistance(ultrasonic1);
+    // float distance2 = getSensorDistance(ultrasonic2);
+    // float distance3 = getSensorDistance(ultrasonic3);
+    // float distance4 = getSensorDistance(ultrasonic4);
+    // Serial.println("Sensor 1 - B4: " + (String)distance1 + " cm");
+    // Serial.println("Sensor 2 - B1: " + (String)distance2 + " cm");
+    // Serial.println("Sensor 3 - A1: " + (String)distance3 + " cm");
+    // Serial.println("Sensor 4 - A2: " + (String)distance4 + " cm");
+    // Serial.println();
+
+    delay(500);
+ 
+}
+
+void waiting_action_exec(){
+  int openGateOutsideButton = digitalRead(outsideButtonPin);
+  int openGateInsideButton = digitalRead(insideButtonPin);
+
+  if(openGateOutsideButton) {
+    buttonPressed = "outside";
+    state = BUTTON_PRESSED;
+  } else if(openGateInsideButton) {
+    buttonPressed = "inside";
+    state = BUTTON_PRESSED;
+  }
+}
+
+void button_pressed_exec() {
+  openGateButtonPressed(buttonPressed);
+  if(buttonPressed == "outside"){
+    state = RUNNING;
+  } else {
+    state = DONE;
+  }
+}
+
+void running_exec() {
+  if(bestSpot.sensorId != -1) {
+    UltraSonicDistanceSensor bestSpotSensor = sensors[bestSpot.sensorId];
+
+    if(!freeParkingSpace(bestSpotSensor)) {
+      Serial.println("A vaga " + bestSpot.name + " foi ocupada!");
+      state = DONE;
+    }
+  } else {
+    state = DONE;
+  }
+}
+
+void done_exec(){
+  state = WAITING_ACTION;
+}
 
 void clearRowLcd(int row) {
   lcd.setCursor(0, row);
@@ -146,7 +256,7 @@ ParkingSpace getBestParkingSpace() {
   }
 }
 
-void openGate(int timeToCLose = 6000){
+void openGate(int timeToCLose = 10000){
   moveGate(1);
   delay(timeToCLose);
   moveGate(0);
@@ -156,7 +266,7 @@ void openGateButtonPressed(String button){
   delay(500);
   if(button == "outside") {
     lcd.setCursor(0, 1);
-    ParkingSpace bestSpot = getBestParkingSpace();
+    bestSpot = getBestParkingSpace();
     if(bestSpot.sensorId != -1){
       lcd.print("Sua vaga eh: "+ bestSpot.name);
       //bestSpot->isFree = false;
@@ -193,49 +303,4 @@ void countParkingSpaces(){
     parkingSpacesCount = count;
     updateParkingSpacesNumber();
   }
-}
-
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(insideButtonPin, INPUT);
-  pinMode(outsideButtonPin, INPUT);
-  servo.attach(servoPin);
-  servo.write(90);
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("EstaciOne");
-}
-
-void loop()
-{     
-    int openGateOutsideButton = digitalRead(outsideButtonPin);
-    int openGateInsideButton = digitalRead(insideButtonPin);
-    ParkingSpace bestSpot = getBestParkingSpace();
-    if(bestSpot.sensorId != -1){
-      Serial.println(bestSpot.path);
-    }
-    
-    if(openGateOutsideButton) {
-      openGateButtonPressed("outside");
-    } else if(openGateInsideButton) {
-      openGateButtonPressed("inside");
-    }
-    
-    countParkingSpaces();
-    
-    
-    // float distance1 = getSensorDistance(ultrasonic1);
-    // float distance2 = getSensorDistance(ultrasonic2);
-    // float distance3 = getSensorDistance(ultrasonic3);
-    // float distance4 = getSensorDistance(ultrasonic4);
-    // Serial.println("Sensor 1 - B4: " + (String)distance1 + " cm");
-    // Serial.println("Sensor 2 - B1: " + (String)distance2 + " cm");
-    // Serial.println("Sensor 3 - A1: " + (String)distance3 + " cm");
-    // Serial.println("Sensor 4 - A2: " + (String)distance4 + " cm");
-    // Serial.println();
-
-    delay(500);
- 
 }
